d96532de1e58347e580e7d0c1028e4f324a17167
samkuria <kuria5614@gmail.com>
2026-07-07 19:58:57 +0300

    Got rid of one navigate function

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -9,7 +9,7 @@ export default function Login() {
   
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const navigate = useNavigate();
+  
   const [showPassword, setShowPassword] = useState(false);
   const [error, setError] = useState(''); // State to hold login errors
 
